a281e8ce48e8a0869032bb3adf3b73fc71f7bc97
balabaev00 <balabaev00@yandex.ru>
2023-05-14 18:16:45 +0900

    feat: Добавление многопоточности

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,65 @@
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private static final int DEFAULT_PAGE_COUNT=1;
+    private static final int DEFAULT_PAGE_COUNT=5;
     private static final String DEFAULT_SUBSTRING= "ЗабГУ";
     private static final FileWorker fileWorker= new FileWorker();
     private static final RegexWorker regexWorker = new RegexWorker();
-    public static void main(String[] args) throws SQLException {
+    private static final int THREAD_COUNT=4;
+    static List<News> news = new ArrayList<>();
+    public static void main(String[] args) throws SQLException, IOException, InterruptedException {
         if (args.length > 0) {
             if (args[0].equals("/parse")) {
                 int pageCount = Integer.parseInt(args[1]);
+
                 Parser parser = new Parser();
-                List<News> news = parser.getNews(pageCount);
+                List<Elements> newsElement = parser.getNewsLine(pageCount);
+                for (int i=0;i<newsElement.size(); i++) {
+                    news.addAll(parser.getNewsFromLine(newsElement.get(i)));
+                }
+
                 fileWorker.saveCsv(news);
             }
         } else {
             Parser parser = new Parser();
-            List<News> news = parser.getNews(DEFAULT_PAGE_COUNT);
+            List<Elements> newsElement = parser.getNewsLine(DEFAULT_PAGE_COUNT);
+
+            // Работа с многопоточностью
+            ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(THREAD_COUNT);
+            for (int i=0; i<newsElement.size(); i++) {
+                int finalI = i;
+                executor.submit(() -> {
+                    try {
+                        List<News> threadNews = parser.getNewsFromLine(newsElement.get(finalI));
+                        synchronized (news) {
+                            news.addAll(threadNews);
+                        }
+
+                        System.out.println("Спарсено " + (finalI+1) + " из " + DEFAULT_PAGE_COUNT + " страниц");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+            executor.shutdown();
+
+            // блокировка основного потока
+            while (true) {
+                boolean result_ = executor.awaitTermination(1, TimeUnit.DAYS);
+
+                if(result_)
+                    break;
+            }
+
+            // Сохраняем все что спарсили
             fileWorker.saveCsv(news);
             saveImages(news);
             saveText(news);
